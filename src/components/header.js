70480151cda9bb106f36b655c52a27bc62529861
Emil Karlsson <emil.karl@gmail.com>
2019-04-14 14:03:31 +0200

    Cleanup in styles and fix header

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,23 @@
 import React from "react";
-
+import iconChevronsDown from "../images/icon-chevrons-down.svg";
 const Header = () => (
   <header className="header">
     <div className="wrapper">
-      <h1>Coinquest</h1>
-      <p>
-        Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus
-        eget urna mollis ornare vel eu leo.
+      <h1 className="header__title">Coinquest</h1>
+      <p className="header__caption">
+        En text om questen lorem ipsum dolor sit amet, consectetur adipiscing
+        elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       </p>
-      <button>View quests</button>
+      <a href="#" className="header__button">
+        <span>View quests</span>
+        <img
+          src={iconChevronsDown}
+          alt="Quests"
+          title="View quests"
+          className="header__button__icon"
+          rel="preload"
+        />
+      </a>
     </div>
   </header>
 );
