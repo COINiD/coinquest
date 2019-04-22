1ae4c2d1785d15071d306d754dc1b436be0aed0e
Emil Karlsson <emil.karl@gmail.com>
2019-04-22 17:21:49 +0200

    Update copy

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,12 +9,12 @@ const Footer = () => (
         on Twitter to learn when new quest and riddles is published.
       </p>
       <p className="footer__copy">
-        2. All riddles will make up a password that you can use on the private
-        key.
+        2. The riddles in a quest will make up a password that you can use on
+        the quests private key to solve it.
       </p>
       <p className="footer__copy">
         3. Use the sweep functionality of your wallet of choice. The private key
-        is a BECH32 address.
+        is a BECH32 address. We recommend COINiD mobile wallet which supports
       </p>
       <p className="footer__copy">
         4. If the password is correct the coins available in the quest will be
