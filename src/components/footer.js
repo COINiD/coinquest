677e6a018a8b73bfb8af4d7558bd06af444ccd12
Emil Karlsson <emil.karl@gmail.com>
2019-05-06 19:09:13 +0200

    Update copy

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,17 +5,21 @@ const Footer = () => (
     <div className="wrapper">
       <h2 className="footer__title">Instructions</h2>
       <p className="footer__copy">
-        1. Each quest includes a number of riddles. Follow this page or COINiD
-        on Twitter to learn when new quest and riddles is published.
+        1. Each quest includes a number of riddles. Follow this page or{" "}
+        <a href="https://twitter.com/coinidgroup">COINiD on Twitter</a> to learn
+        when new quest and riddles is published.
       </p>
       <p className="footer__copy">
         2. The riddles in a quest will make up a password that you can use on
         the quests private key to solve it.
       </p>
       <p className="footer__copy">
-        3. Use the sweep functionality of your wallet of choice. The private key
-        is a BECH32 address. We recommend COINiD mobile wallet which supports
-        sweep private key of BECH32 addresses.
+        3. Use the sweep functionality of your wallet of choice, the wallet must
+        be able to sweep private keys with Bech32 addresses. We recommend{" "}
+        <a href="https://coinid.org/#download-now" target="_blank">
+          COINiD mobile wallet
+        </a>{" "}
+        which supports sweep private key of Bech32 addresses.
       </p>
       <p className="footer__copy">
         4. If the password is correct the coins available in the quest will be
