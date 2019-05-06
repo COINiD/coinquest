ccc750968cd840ea5ab6c84f390cbb4fa8271672
Emil Karlsson <emil.karl@gmail.com>
2019-05-06 21:55:40 +0200

    Add 404

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from "react";
+import Layout from "../components/layout";
+
+export default class Page extends PureComponent {
+  render() {
+    return (
+      <Layout title="CoinQuest" theme="dark">
+        <div className="wrapper">404</div>
+      </Layout>
+    );
+  }
+}
