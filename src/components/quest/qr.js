a7233853e2f6296d87ea9b0a113547179ed4b4b2
Emil Karlsson <emil.karl@gmail.com>
2019-05-06 21:54:04 +0200

    Clickable QR

--- src/components/quest/qr.js
+++ src/components/quest/qr.js
@@ -2,14 +2,14 @@ import React from "react";
 import { QRCode } from "react-qr-svg";
 
 const Qr = ({ privateKey }) => (
-  <div className="quest__qr">
+  <a href={`coinid://sweep/${privateKey}`} className="quest__qr">
     <QRCode
       cellClassPrefix="quest__qr-"
       level="Q"
       style={{ width: 168 }}
-      value={`coinid://sweep/${privateKey}`}
+      value={privateKey}
     />
-  </div>
+  </a>
 );
 
 export default Qr;
