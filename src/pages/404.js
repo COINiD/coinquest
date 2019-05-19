43cafb9cfcee528dae384725e44279581c855858
Emil Karlsson <emil.karl@gmail.com>
2019-05-19 22:39:05 +0200

    Update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,14 @@ export default class Page extends PureComponent {
   render() {
     return (
       <Layout title="CoinQuest" theme="dark" simpleHeader={true}>
-        <div className="wrapper">404</div>
+        <div className="wrapper">
+          <h2>Page not found</h2>
+          <h3 className="u-margin-bottom">
+            The page you were looking for does not exist. Visit{" "}
+            <a href="https://coinid.org">COINiD</a> or go to the{" "}
+            <a href="/">startpage</a>.
+          </h3>
+        </div>
       </Layout>
     );
   }
