995387f769b0ff86dccb0aad7aacbf49d164cfef
Emil Karlsson <emil.karl@gmail.com>
2019-05-21 20:24:50 +0200

    Update instructions

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,31 +7,30 @@ const Footer = () => (
   <footer className="footer">
     <div className="wrapper">
       <h2 className="footer__title">Instructions</h2>
-      <p className="footer__copy">
-        1. Each quest includes a number of riddles. Follow this page or{" "}
-        <a href="https://twitter.com/coinidgroup">COINiD on Twitter</a> to learn
-        when new quest and riddles is published.
-      </p>
-      <p className="footer__copy">
-        2. The riddles in a quest will make up a password that you can use on
-        the quests private key to solve it.
-      </p>
-      <p className="footer__copy">
-        3. Use the sweep functionality of your wallet of choice, the wallet must
-        be able to sweep private keys with Bech32 addresses. We recommend{" "}
-        <a
-          href="https://coinid.org/#download-now"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          COINiD mobile wallet
-        </a>{" "}
-        which supports sweep private key of Bech32 addresses.
-      </p>
-      <p className="footer__copy">
-        4. If the password is correct the coins available in the quest will be
-        directly transferred to your wallet.
-      </p>
+      <ol className="footer__list">
+        <li className="footer__list-item">
+          Each quest includes a number of riddles. Follow this page or{" "}
+          <a href="https://twitter.com/coinidgroup">COINiD on Twitter</a> to
+          learn when new quest and riddles is published.
+        </li>
+        <li className="footer__list-item">
+          Download and{" "}
+          <a href="https://coinid.org/guides/hot-wallet">
+            setup a COINiD wallet
+          </a>{" "}
+          for the quests coin.
+        </li>
+        <li className="footer__list-item">
+          Go to receive in the wallet app and select <i>Sweep private key</i>{" "}
+          and scan QR code for the quest.
+        </li>
+        <li className="footer__list-item">
+          Hint will lead you to a quest page where you find the riddles.
+        </li>
+        <li className="footer__list-item">
+          Solve the riddles to get the password
+        </li>
+      </ol>
       <p className="footer__downloads">
         <WalletButton system="ios" />
         <WalletButton system="android" />
