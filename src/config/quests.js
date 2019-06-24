77e81127125d320f4051d49c1d84ef0ba00a315d
Emil Karlsson <emil.karl@gmail.com>
2019-06-24 20:51:48 +0200

    Update address and QR

--- src/config/quests.js
+++ src/config/quests.js
@@ -2,7 +2,7 @@ export default [
   {
     id: 1,
     code: "308176978bda5cb37cc465721c80da3e",
-    address: "MW25FEGZTTM7agrH2bES16t6Vqg4NuZ7UF",
+    address: "my1qrv0vu4pm7xrnr99q5xlfk69adj5fzvkz0544de",
     title: "Summer is coming",
     description:
       "Scan the QR code with sweep in COINiD Wallet for Myriad to access the quest",
