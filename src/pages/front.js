24c0ff385749b89aff99b32b2bc8a8430d507331
Emil Karlsson <emil.karl@gmail.com>
2019-06-24 21:02:08 +0200

    Add empty state

--- src/pages/front.js
+++ src/pages/front.js
@@ -3,13 +3,36 @@ import withQuests from "../enhancers/quests";
 import QuestList from "../components/quest-list";
 
 class Page extends PureComponent {
-  render() {
-    let { activeQuests } = this.props;
+  renderList(quests) {
+    return <QuestList quests={quests} />;
+  }
+
+  renderEmpty() {
     return (
-      <div className="quests u-padding-top" id="quests">
-        <QuestList quests={activeQuests} />
+      <div className="empty-state">
+        No active quests at the moment.<br />Keep your out on channels for the
+        next one!
       </div>
     );
+  }
+
+  render() {
+    let { activeQuests } = this.props;
+    if (activeQuests) {
+      return (
+        <div className="quests u-padding-top" id="quests">
+          {activeQuests.length > 0
+            ? this.renderList(activeQuests)
+            : this.renderEmpty()}
+        </div>
+      );
+    } else {
+      return (
+        <div className="quests quests--empty">
+          No available quests at the moment. Keep your eye out for one!
+        </div>
+      );
+    }
   }
 }
 
